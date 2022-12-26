e2bec38dbe3a7645a633b45a5a2609bce2006a07
NeoRevolt <dzalfikriali@gmail.com>
2022-12-26 15:17:40 +0700

    Implement drawimage

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.neorevolt.drawimage.EditImageActivity
 import com.neorevolt.my_remote_library.Bekasi
 import com.neorevolt.my_remote_library.DetailLibraryActivity
 
@@ -15,15 +16,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val jalan = Bekasi.jalan[1]
+        val jalan = Bekasi.jalan[0]
 
         binding.apply {
             btnToLib.setOnClickListener {
-                Intent(
-                    this@MainActivity,
-                    Class.forName("com.neorevolt.my_remote_library.DetailLibraryActivity")
-                ).also {
-                    it.putExtra(DetailLibraryActivity.EXTRA_DETAIL, jalan)
+//                Intent(
+//                    this@MainActivity,
+//                    Class.forName("com.neorevolt.my_remote_library.DetailLibraryActivity")
+//                ).also {
+//                    it.putExtra(DetailLibraryActivity.EXTRA_DETAIL, jalan)
+//                    startActivity(it)
+//                }
+                Intent(this@MainActivity, Class.forName("com.neorevolt.drawimage.EditImageActivity")).also {
+                    it.putExtra(EditImageActivity.EXTRA_REQ,"remote")
+                    it.putExtra(EditImageActivity.EXTRA_PHOTO,"https://w0.peakpx.com/wallpaper/205/460/HD-wallpaper-sea-beach-beautiful-scene-clouds-deep-sea-natural-graphy-nature-beauty-graphy-lover-portrait.jpg")
                     startActivity(it)
                 }
             }
